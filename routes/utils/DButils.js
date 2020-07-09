ed2ba0920426fb8cbfc096b423e0afbd1ac9383f
Chen <chenavra@post.bgu.ac.il>
2020-07-09 21:16:14 +0300

    chen

---
+++ routes/utils/DButils.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const sql = require("mssql");
+
+const config = {
+  user: process.env.tedious_userName,
+  password: process.env.tedious_password,
+  server: process.env.tedious_server,
+  database: process.env.tedious_database,
+  options: {
+    encrypt: true,
+    enableArithAbort: true
+  }
+};
+
+exports.execQuery = async function (query) {
+  var pool = undefined;
+  var result = undefined;
+  try {
+    pool = await sql.connect(config);
+    result = await pool.request().query(query);
+    return result.recordset;
+  } catch (error) {
+    throw error;
+  } finally {
+    if (pool) pool.close();
+  }
+};
+
